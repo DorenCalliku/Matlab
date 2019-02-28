cf52343eb0235be5d6023d4c9a4e5765d47f16cd
Doren Calliku <dcalliku@gmail.com>
2019-02-28 22:08:09 +0300

    Create groups.m

---
+++ groups.m
@@ -0,0 +1,3 @@
+%% This will be a messed up functionality which should get together different properties of groups of people. 
+% To be defined: group dynamics, group size, group directions. 
+% Good luck fucker!
