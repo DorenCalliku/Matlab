973483cb3948b3343c0f697d16633286b2a3c33c
Doren Calliku <dcalliku@gmail.com>
2019-03-03 10:52:12 +0300

    Delete groups.m

--- groups.m
+++
@@ -1,3 +0,0 @@
-%% This will be a messed up functionality which should get together different properties of groups of people. 
-% To be defined: group dynamics, group size, group directions. 
-% Good luck fucker!
